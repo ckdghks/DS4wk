bfab95b7c99495a51bc7d266a65bd08e93ef72df
창환 <63768572+ckdghks@users.noreply.github.com>
2021-09-23 13:50:43 +0000

    4번 문제 문자열 출력 변경

--- main.c
+++ main.c
@@ -1,30 +1,24 @@
+/*
+크기가 10인 배열 two[]를 선언하고 여기에 2의 제곱 값들을 저장해보자. 즉 배열의 첫 번째 요소에는 2^0을 저장하고 두 번째 요소에는 2^1값을 저장한다. 마지막 요소에는 2^9값을 저장한다. for루프를 이용하여 two[] 배열의 전체 요소의 값을 출력하는 프로그램을 작성하라.
+*/
 #include <stdio.h>
 #define COL 10
 
-typedef struct complex {
-  float real, imaginary;
-} COMPLEX;
-
-COMPLEX complex_add(COMPLEX a, COMPLEX b);
-
 int main(void) {
-  COMPLEX c1, c2, c3;
+  int two[COL] = {0};
+  int i;
 
-  c1.real = 1;
-  c1.imaginary =2;
-  c2.real = 3;
-  c2.imaginary = 4;
+  for(i=0; i<COL; i++){
+    int exp = 1;
+    for(int j=0; j<i; j++){
+      exp = exp * 2;
+    }
+    two[i] = exp;
+  }
 
-  c3 = complex_add(c1, c2);
-  printf("%0.2f+%0.2fi\n", c3.real, c3.imaginary);
-  
+  for(i=0; i<COL; i++){
+    printf("2^%d: %d\n", i, two[i]);
+  }
+
   return 0;
-}
-
-COMPLEX complex_add(COMPLEX a, COMPLEX b){
-  COMPLEX c;
-  c.real = a.real + b.real;
-  c.imaginary = a.imaginary + b.imaginary;
-
-  return c;
 }
--- person.c
+++ person.c
@@ -1,14 +1,19 @@
 /*
 #include <stdio.h>
-#define COL 10
 
 struct person {
-  char name[COL];
+  char* name;
   int age;
   float wage;
 };
 
 int main(void) {
+  struct person p1;
+  p1.name = "홍길동";
+  p1.age = 21;
+  p1.wage = 10000;
+
+  printf("이름은 %s 나이는 %d 임금은 %.2f", p1.name, p1.age, p1.wage);
 
   return 0;
 }
